3384050ba866e3883fda82f912ec71da8e9b848e
AKhanolkar14 <115803659+AKhanolkar14@users.noreply.github.com>
2023-08-21 17:48:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function createCanvas()
+{
+
+}
+function preload(){
+
+}
+
+function setup(){
+    canvas= createCanvas(640, 480);
+    canvas.position(110, 250);
+    video= createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    Image(video,0,0,640,480)
+}
+
+function take_snapshot(){
+    save('student_name.png');
+}
